a8a311d66cc95f5e5e83058f7e237d19fb35a8a5
marcelobruckner <marcelobruckner@gmail.com>
2020-07-03 15:03:36 -0300

    commit inicial do projeto

---
+++ src/main/java/br/com/digitalinnovation/one/RestfulJaxrsApplication.java
@@ -0,0 +1,13 @@
+package br.com.digitalinnovation.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulJaxrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulJaxrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/digitalinnovation/one/RestfulJaxrsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digitalinnovation.one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulJaxrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
